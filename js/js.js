848061eeca9fd9e80c6b80116ea8a477ab676ea6
mimichigua <86659500+mimichigua@users.noreply.github.com>
2021-07-13 23:31:08 +0800

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,29 @@
+
+		function showduyin(){
+			document.getElementById('mm').style.display = 'block';
+			document.getElementById('strhref').innerHTML="hide";
+			document.getElementById('strhref').href="javascript:hideduyin()";
+		}
+		function hideduyin(){
+			document.getElementById('mm').style.display = 'none';
+			document.getElementById('strhref').innerHTML="show";
+			document.getElementById('strhref').href="javascript:showduyin()";
+ 
+		}
+		
+		
+		
+		function showshiyi(){
+			document.getElementById('nn').style.display = 'block';
+			document.getElementById('getshiyi').innerHTML="hide";
+			document.getElementById('getshiyi').href="javascript:hideshiyi()";
+		}
+		function hideshiyi(){
+			document.getElementById('nn').style.display = 'none';
+			document.getElementById('getshiyi').innerHTML="show";
+			document.getElementById('getshiyi').href="javascript:showshiyi()";
+ 
+		}
+		
+		
+		
